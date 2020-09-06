f8fd79dbeae8ecdf501a1a25b2a6f2e970981aa9
Jonas Svensson <github@jonass.user.lysator.liu.se>
2020-09-06 15:48:12 +0200

    Split monolith source to files.

---
+++ platser.go
@@ -0,0 +1,274 @@
+//-*- coding: utf-8 -*-
+
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	
+	_ "github.com/alexbrainman/odbc"    // BSD-3-Clause License 
+	_ "github.com/mattn/go-sqlite3"     // MIT License
+)
+
+func printPlatser(w http.ResponseWriter, db *sql.DB) {
+	res, err := db.Query("SELECT Namn,Gironummer,Typ,RefKonto,Löpnr FROM Platser")
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+
+	var Namn []byte  // size 40
+	var Gironummer []byte  // size 20
+	var Typ []byte  // size 2
+	var RefKonto []byte  // size 40
+	var Löpnr []byte  // autoinc Primary Key, index
+
+	fmt.Fprintf(w, "<table style=\"width:100%%\"><tr><th>Namn</th><th>Gironummer</th><th>Typ</th><th>RefKonto</th><th>Redigera</th><th>Radera</th>\n")
+	for res.Next() {
+		err = res.Scan(&Namn,&Gironummer,&Typ,&RefKonto,&Löpnr)
+
+		fmt.Fprintf(w, "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td>", toUtf8(Namn), toUtf8(Gironummer), toUtf8(Typ), toUtf8(RefKonto))
+		fmt.Fprintf(w, "<td><form method=\"POST\" action=\"/platser\"><input type=\"hidden\" id=\"lopnr\" name=\"lopnr\" value=\"%s\"><input type=\"hidden\" id=\"action\" name=\"action\" value=\"editform\"><input type=\"submit\" value=\"Redigera\"></form></td>\n", Löpnr)
+		fmt.Fprintf(w, "<td><form method=\"POST\" action=\"/platser\"><input type=\"hidden\" id=\"lopnr\" name=\"lopnr\" value=\"%s\"><input type=\"hidden\" id=\"action\" name=\"action\" value=\"radera\"><input type=\"checkbox\" id=\"OK\" name=\"OK\" required><label for=\"OK\">OK</label><input type=\"submit\" value=\"Radera\"></form></td></tr>\n", Löpnr)
+	}
+	fmt.Fprintf(w, "</table>\n")
+
+	fmt.Fprintf(w, "<form method=\"POST\" action=\"/platser\"><input type=\"hidden\" id=\"action\" name=\"action\" value=\"addform\"><input type=\"submit\" value=\"Ny plats\"></form>\n")
+}
+
+func printPlatserFooter(w http.ResponseWriter, db *sql.DB) {
+	fmt.Fprintf(w, "<a href=\"summary\">Översikt</a>\n")
+	fmt.Fprintf(w, "</body>\n")
+	fmt.Fprintf(w, "</html>\n")
+}
+
+func raderaPlats(w http.ResponseWriter, lopnr int, db *sql.DB) {
+	fmt.Println("raderaPlats lopnr: ", lopnr)
+	
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	_, err := db.ExecContext(ctx,
+`DELETE FROM Platser WHERE (Löpnr=?)`, lopnr)
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+	fmt.Fprintf(w, "Plats med löpnr %d raderad.<br>", lopnr);
+}
+
+func editformPlats(w http.ResponseWriter, lopnr int, db *sql.DB) {
+	fmt.Println("editformPlats lopnr: ", lopnr)
+
+	kontonamn := getAccNames()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	res1 := db.QueryRowContext(ctx,
+`SELECT Namn,Gironummer,Typ,RefKonto FROM Platser WHERE (Löpnr=?)`, lopnr)
+
+	var Namn []byte  // size 40
+	var Gironummer []byte  // size 20
+	var Typ []byte  // size 2
+	var RefKonto []byte  // size 40
+	err := res1.Scan(&Namn,&Gironummer,&Typ,&RefKonto)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+
+	fmt.Fprintf(w, "Redigera plats<br>")
+	fmt.Fprintf(w, "<form method=\"POST\" action=\"/platser\">")
+
+	fmt.Fprintf(w,"<label for=\"namn\">Namn:</label>")
+	fmt.Fprintf(w,"<input type=\"text\" id=\"namn\" name=\"namn\" value=\"%s\">", toUtf8(Namn))
+	fmt.Fprintf(w,"<label for=\"gironum\">Gironummer:</label>")
+	fmt.Fprintf(w,"<input type=\"text\" id=\"gironum\" name=\"gironum\" value=\"%s\">", toUtf8(Gironummer))
+	fmt.Fprintf(w,"<label for=\"type\">Typ:</label>")
+	fmt.Fprintf(w,"<input type=\"text\" id=\"type\" name=\"type\" value=\"%s\">", toUtf8(Typ))
+	fmt.Fprintf(w,"<label for=\"refacc\">RefKonto:</label>")
+	fmt.Fprintf(w,"<select id=\"refacc\" name=\"refacc\">")
+	for _,s := range kontonamn {
+		var selected string = ""
+		if s == toUtf8(RefKonto) {
+			selected="selected"
+		}
+		fmt.Fprintf(w,"    <option value=\"%s\" %s>%s</option>", s, selected, s)
+	}
+
+	fmt.Fprintf(w,"</select>\n")
+	
+	fmt.Fprintf(w, "<input type=\"hidden\" id=\"lopnr\" name=\"lopnr\" value=\"%d\">", lopnr)
+	fmt.Fprintf(w, "<input type=\"hidden\" id=\"action\" name=\"action\" value=\"update\">")
+	fmt.Fprintf(w, "<input type=\"submit\" value=\"Uppdatera\">")
+	fmt.Fprintf(w, "</form>\n")
+	fmt.Fprintf(w, "<p>\n");
+}
+
+func addformPlats(w http.ResponseWriter, db *sql.DB) {
+	fmt.Println("addformPlats ")
+
+	kontonamn := getAccNames()
+
+	fmt.Fprintf(w, "Lägg till plats<br>")
+	fmt.Fprintf(w, "<form method=\"POST\" action=\"/platser\">")
+
+	fmt.Fprintf(w,"<label for=\"namn\">Namn:</label>")
+	fmt.Fprintf(w,"<input type=\"text\" id=\"namn\" name=\"namn\" value=\"%s\">", "")
+	fmt.Fprintf(w,"<label for=\"gironum\">Gironummer:</label>")
+	fmt.Fprintf(w,"<input type=\"text\" id=\"gironum\" name=\"gironum\" value=\"%s\">", "")
+	fmt.Fprintf(w,"<label for=\"kontokort\">Kontokortsföretag:</label>")
+	fmt.Fprintf(w,"<input type=\"checkbox\" id=\"kontokort\" name=\"kontokort\">")
+	fmt.Fprintf(w,"<label for=\"refacc\">RefKonto:</label>")
+	fmt.Fprintf(w,"<select id=\"refacc\" name=\"refacc\">")
+	for _,s := range kontonamn {
+		var selected string = ""
+		fmt.Fprintf(w,"    <option value=\"%s\" %s>%s</option>", s, selected, s)
+	}
+
+	fmt.Fprintf(w,"</select>\n")
+	
+	fmt.Fprintf(w, "<input type=\"hidden\" id=\"action\" name=\"action\" value=\"add\">")
+	fmt.Fprintf(w, "<input type=\"submit\" value=\"Ny plats\">")
+	fmt.Fprintf(w, "</form>\n")
+	fmt.Fprintf(w, "<p>\n");
+}
+
+func addPlats(w http.ResponseWriter, namn string, gironum string, acctype bool, refacc string, db *sql.DB) {
+	fmt.Println("addPlats namn: ", namn)
+	
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if len(gironum) < 1 {
+		gironum = " "
+	}
+	
+	if !acctype {
+		refacc=" "
+	}
+	
+	_, err := db.ExecContext(ctx,
+`INSERT INTO Platser (Namn,Gironummer,Typ,RefKonto) VALUES (?, ?, ?, ?)`, namn, gironum, "", refacc)
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+	fmt.Fprintf(w, "Plats %s tillagd.<br>", namn);
+}
+
+
+func updatePlats(w http.ResponseWriter, lopnr int, namn string, gironum string, acctype string, refacc string, db *sql.DB) {
+	fmt.Println("updatePlats lopnr: ", lopnr)
+	
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	_, err := db.ExecContext(ctx,
+`UPDATE Platser SET Namn = ?, Gironummer = ?, Typ = ?, RefKonto = ? WHERE (Löpnr=?)`, namn, gironum, acctype, refacc, lopnr)
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+	fmt.Fprintf(w, "Plats %s uppdaterad.<br>", namn);
+}
+
+func hanteraplatser(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "<html>\n")
+	fmt.Fprintf(w, "<head>\n")
+	fmt.Fprintf(w, "<style>\n")
+	fmt.Fprintf(w, "table,th,td { border: 1px solid black }\n")
+	fmt.Fprintf(w, "</style>\n")
+	fmt.Fprintf(w, "</head>\n")
+	fmt.Fprintf(w, "<body>\n")
+
+	fmt.Fprintf(w, "<h1>%s</h1>\n", currentDatabase)
+	fmt.Fprintf(w, "<h2>Platser</h2>\n")
+
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	formaction := req.FormValue("action")
+	var lopnr int=-1;
+	if len(req.FormValue("lopnr"))>0 {
+		lopnr,err = strconv.Atoi(req.FormValue("lopnr"))
+	}
+
+	switch formaction {
+	case "radera" : raderaPlats(w, lopnr, db)
+	case "addform" : addformPlats(w, db)
+	case "add" :
+		var namn string="";
+		if len(req.FormValue("namn"))>0 {
+			namn = req.FormValue("namn")
+		}
+		var gironum string="";
+		if len(req.FormValue("gironum"))>0 {
+			gironum = req.FormValue("gironum")
+		}
+		var acctype bool=false;
+		fmt.Println("FormValue type: ", req.FormValue("kontokort"))
+		if req.FormValue("kontokort")== "on" {
+			acctype = true
+		}
+		var refacc string="";
+		if len(req.FormValue("refacc"))>0 {
+			refacc = req.FormValue("refacc")
+		}
+		addPlats(w, namn, gironum, acctype, refacc, db)
+	case "editform" : editformPlats(w, lopnr, db)
+	case "update" :
+		var namn string="";
+		if len(req.FormValue("namn"))>0 {
+			namn = req.FormValue("namn")
+		}
+		var gironum string="";
+		if len(req.FormValue("gironum"))>0 {
+			gironum = req.FormValue("gironum")
+		}
+		var acctype string="";
+		if len(req.FormValue("type"))>0 {
+			acctype = req.FormValue("type")
+		}
+		var refacc string="";
+		if len(req.FormValue("refacc"))>0 {
+			refacc = req.FormValue("refacc")
+		}
+		updatePlats(w, lopnr, namn, gironum, acctype, refacc, db)
+	default :
+		fmt.Println("Okänd action: %s\n", formaction)
+	}
+	printPlatser(w, db)
+	printPlatserFooter(w, db)
+}
+
+func getPlaceNames() []string {
+	names := make([]string, 0)
+
+	res, err := db.Query("SELECT Namn FROM Platser ORDER BY Namn")
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+
+	var Namn  []byte  // size 40, index
+	for res.Next() {
+		err = res.Scan(&Namn)
+		names = append(names, toUtf8(Namn))
+	}
+	return names
+}
+
---
+++ transaktioner.go
@@ -0,0 +1,393 @@
+//-*- coding: utf-8 -*-
+
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"strconv"
+	"time"
+	
+	_ "github.com/alexbrainman/odbc"    // BSD-3-Clause License 
+	_ "github.com/mattn/go-sqlite3"     // MIT License
+)
+
+func printTransactions(w http.ResponseWriter, db *sql.DB, startDate string, endDate string, limitcomment string) {
+	fmt.Println("printTransactions startDate:", startDate)
+	fmt.Println("printTransactions endDate:", endDate)
+	fmt.Println("printTransactions comment:", limitcomment)
+	
+	fmt.Fprintf(w, "<h1>%s</h1>\n", currentDatabase)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	var err error
+	var res *sql.Rows
+
+	if len(limitcomment)>0 {
+		res, err = db.QueryContext(ctx,
+`SELECT FrånKonto,TillKonto,Typ,Datum,Vad,Vem,Belopp,Löpnr,Saldo,Fastöverföring,Text from transaktioner
+  where (datum < ?) and (datum >= ?) and (Text like ?)
+order by datum,löpnr`, endDate, startDate, limitcomment)
+	} else {
+		res, err = db.QueryContext(ctx,
+`SELECT FrånKonto,TillKonto,Typ,Datum,Vad,Vem,Belopp,Löpnr,Saldo,Fastöverföring,Text from transaktioner
+  where (datum < ?) and (datum >= ?)
+order by datum,löpnr`, endDate, startDate)
+	}
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+
+	var fromAcc []byte  // size 40
+	var toAcc []byte    // size 40
+	var tType []byte    // size 40
+	var date []byte     // size 10
+	var what []byte     // size 40
+	var who []byte      // size 50
+	var amount []byte   // BCD / Decimal Precision 19
+	var nummer int      // Autoinc Primary Key, index
+	var saldo []byte    // BCD / Decimal Precision 19
+	var fixed bool      // Boolean
+	var comment []byte  // size 60
+
+	fmt.Fprintf(w, "<table style=\"width:100%%\"><tr><th>Löpnr</th><th>Frånkonto</th><th>Tillkonto/Plats</th><th>Typ</th><th>Vad</th><th>Datum</th><th>Vem</th><th>Belopp</th><th>Text</th><th>Fast överföring</th>\n")
+	for res.Next() {
+		err = res.Scan(&fromAcc, &toAcc, &tType, &date, &what, &who, &amount, &nummer, &saldo, &fixed, &comment)
+		
+		sqlStmt:=""
+		sqlStmt+="<tr><td>" + strconv.Itoa(nummer) + "</td>"
+		sqlStmt+="<td>" + toUtf8(fromAcc) + "</td>"
+		sqlStmt+="<td>" + toUtf8(toAcc) + "</td>"
+		sqlStmt+="<td>" + toUtf8(tType) + "</td>"
+		sqlStmt+="<td>" + toUtf8(what) + "</td>"
+		sqlStmt+="<td>" + toUtf8(date) + "</td>"
+		sqlStmt+="<td>" + toUtf8(who) + "</td>"
+		sqlStmt+="<td>" + toUtf8(amount) + "</td>"
+		sqlStmt+="<td>" + escapeHTML(toUtf8(comment)) + "</td>\n"
+		sqlStmt+="<td>" + strconv.FormatBool(fixed) + "</td></tr>"
+		fmt.Fprintf(w, sqlStmt)
+	}
+	fmt.Fprintf(w, "</table>\n")
+}
+
+func transactions(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "<html>\n")
+	fmt.Fprintf(w, "<head>\n")
+	fmt.Fprintf(w, "<style>\n")
+	fmt.Fprintf(w, "table,th,td { border: 1px solid black }\n")
+	fmt.Fprintf(w, "</style>\n")
+	fmt.Fprintf(w, "</head>\n")
+	fmt.Fprintf(w, "<body>\n")
+
+	currentTime := time.Now()
+	startDate := currentTime.Format("2006-01-02")
+	startDate = startDate[0:8] + "01"
+	endDay := currentTime.AddDate(0, 1, 0)
+	endDate := endDay.Format("2006-01-02")
+	endDate = endDate[0:8] + "01"
+
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(req.FormValue("startdate"))>3 {
+		startDate = req.FormValue("startdate")
+	}
+	if len(req.FormValue("enddate"))>3 {
+		endDate = req.FormValue("enddate")
+	}
+
+	if(db==nil) {
+		fmt.Fprintf(w, "Transactions: No database open<p>\n")
+	} else {
+		res1 := db.QueryRow("SELECT MIN(Datum) FROM Transaktioner")
+		var date []byte     // size 10
+		err = res1.Scan(&date)
+		res1 = db.QueryRow("SELECT MAX(Datum) FROM Transaktioner")
+		err = res1.Scan(&date)
+
+		printTransactions(w, db, startDate, endDate, req.FormValue("comment"))
+		fmt.Fprintf(w,"<form method=\"POST\" action=\"/transactions\">\n")
+		fmt.Fprintf(w,"<label for=\"startdate\">Startdatum:</label>")
+		fmt.Fprintf(w,"	<input type=\"date\" id=\"startdate\" name=\"startdate\" value=\"%s\" title=\"Inklusive\">", startDate)
+		fmt.Fprintf(w,"<label for=\"enddate\">Slutdatum:</label>")
+		fmt.Fprintf(w,"	<input type=\"date\" id=\"enddate\" name=\"enddate\" value=\"%s\" title=\"Exclusive\">", endDate)
+		fmt.Fprintf(w,"<label for=\"comment\">Kommentar:</label>")
+		fmt.Fprintf(w,"	<input id=\"comment\" name=\"comment\" value=\"%s\" placeholder=\"wildcards %%_\" title=\"Söktext\n%% är noll, ett eller många tecken.\n_ är ett tecken.\nTomt fält betyder inget filtreras.\">", req.FormValue("comment"))
+
+		fmt.Fprintf(w,"<input type=\"submit\" value=\"Visa\"></form>\n")
+
+		fmt.Fprintf(w,"<form method=\"POST\" action=\"/transactions\">\n")
+	}
+	
+	fmt.Fprintf(w, "<a href=\"summary\">Översikt</a>\n")
+	fmt.Fprintf(w, "</body>\n")
+	fmt.Fprintf(w, "</html>\n")
+}
+
+func newtransaction(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("newtrans 10")
+	// Common
+	kontonamn := getAccNames()
+	fmt.Println("newtrans 20")
+	platser := getPlaceNames()
+	personer := getPersonNames()
+	vad_inkomst := getTypeInNames()
+	vad_utgift := getTypeOutNames()
+	fmt.Println("newtrans 30")
+	
+	fmt.Fprintf(w, "<html>\n")
+	fmt.Fprintf(w, "<head>\n")
+	fmt.Fprintf(w, "<style>\n")
+	fmt.Fprintf(w, "table,th,td { border: 1px solid black }\n")
+	fmt.Fprintf(w, "</style>\n")
+	fmt.Fprintf(w, "</head>\n")
+	fmt.Fprintf(w, "<body>\n")
+	fmt.Fprintf(w, "<h1>%s</h1>\n", currentDatabase)
+	
+	// Inköp
+	fmt.Fprintf(w, "<h3>Inköp</h3>\n")
+	fmt.Fprintf(w,"<form method=\"POST\" action=\"/addtrans\">\n")
+	fmt.Fprintf(w,"<input type=\"hidden\" id=\"transtyp\" name=\"transtyp\" value=\"Inköp\">\n")
+	fmt.Fprintf(w,"  <label for=\"fromacc\">Från:</label>")
+	fmt.Fprintf(w,"  <select name=\"fromacc\" id=\"fromacc\">")
+	for _,s := range kontonamn {
+		fmt.Fprintf(w,"    <option value=\"%s\">%s</option>", s, s)
+	}
+	fmt.Println("newtrans 40")
+	fmt.Fprintf(w,"  </select>\n")
+	fmt.Fprintf(w,"  <label for=\"place\">Plats:</label>")
+	fmt.Fprintf(w,"  <select name=\"place\" id=\"place\">")
+	for _,s := range platser {
+		fmt.Fprintf(w,"    <option value=\"%s\">%s</option>", s, s)
+	}
+	fmt.Fprintf(w,"  </select>\n")
+	fmt.Fprintf(w,"<label for=\"date\">Datum:</label>")
+	fmt.Fprintf(w,"	<input type=\"date\" id=\"date\" name=\"date\">")
+	fmt.Fprintf(w,"  <label for=\"what\">Vad:</label>")
+	fmt.Fprintf(w,"  <select name=\"what\" id=\"what\">")
+	for _,s := range vad_utgift {
+		fmt.Fprintf(w,"    <option value=\"%s\">%s</option>", s, s)
+	}
+	fmt.Fprintf(w,"  </select>\n")
+	fmt.Fprintf(w,"  <label for=\"who\">Vem:</label>")
+	fmt.Fprintf(w,"  <select name=\"who\" id=\"who\">")
+	for _,s := range personer {
+		fmt.Fprintf(w,"    <option value=\"%s\">%s</option>", s, s)
+	}
+	fmt.Fprintf(w,"  </select>\n")
+	fmt.Fprintf(w,"<label for=\"amount\">Belopp:</label>")
+	fmt.Fprintf(w,"<input type=\"number\" id=\"amount\" name=\"amount\" min=0 step=\"0.01\">")
+	fmt.Fprintf(w,"<label for=\"text\">Text:</label>")
+	fmt.Fprintf(w,"<input type=\"text\" id=\"text\" name=\"text\" >")
+	 fmt.Fprintf(w,"<input type=\"submit\" value=\"Submit\"></form>\n")
+	// Insättning
+	fmt.Fprintf(w, "<h3>Insättning</h3>\n")
+	fmt.Fprintf(w,"<form method=\"POST\" action=\"/addtrans\">\n")
+	fmt.Fprintf(w,"<input type=\"hidden\" id=\"transtyp\" name=\"transtyp\" value=\"Insättning\">\n")
+	fmt.Fprintf(w,"  <label for=\"toacc\">Till:</label>")
+	fmt.Fprintf(w,"  <select name=\"toacc\" id=\"fromacc\">")
+	for _,s := range kontonamn {
+		fmt.Fprintf(w,"    <option value=\"%s\">%s</option>", s, s)
+	}
+	fmt.Println("newtrans 40")
+	fmt.Fprintf(w,"  </select>\n")
+	fmt.Fprintf(w,"<label for=\"date\">Datum:</label>")
+	fmt.Fprintf(w,"	<input type=\"date\" id=\"date\" name=\"date\">")
+	fmt.Fprintf(w,"  <label for=\"what\">Vad:</label>")
+	fmt.Fprintf(w,"  <select name=\"what\" id=\"what\">")
+	for _,s := range vad_inkomst {
+		fmt.Fprintf(w,"    <option value=\"%s\">%s</option>", s, s)
+	}
+	fmt.Fprintf(w,"  </select>\n")
+	fmt.Fprintf(w,"  <label for=\"who\">Vem:</label>")
+	fmt.Fprintf(w,"  <select name=\"who\" id=\"who\">")
+	for _,s := range personer {
+		fmt.Fprintf(w,"    <option value=\"%s\">%s</option>", s, s)
+	}
+	fmt.Fprintf(w,"  </select>\n")
+	fmt.Fprintf(w,"<label for=\"amount\">Belopp:</label>")
+	fmt.Fprintf(w,"<input type=\"number\" id=\"amount\" name=\"amount\" min=0 step=\"0.01\">")
+	fmt.Fprintf(w,"<label for=\"text\">Text:</label>")
+	fmt.Fprintf(w,"<input type=\"text\" id=\"text\" name=\"text\" >")
+	fmt.Fprintf(w,"<input type=\"submit\" value=\"Submit\"></form>\n")
+	// Uttag
+	fmt.Fprintf(w, "<h3>Uttag</h3>\n")
+	fmt.Fprintf(w,"<form method=\"POST\" action=\"/addtrans\">\n")
+	fmt.Fprintf(w,"<input type=\"hidden\" id=\"transtyp\" name=\"transtyp\" value=\"Uttag\">\n")
+	fmt.Fprintf(w,"  <label for=\"fromacc\">Från:</label>")
+	fmt.Fprintf(w,"  <select name=\"fromacc\" id=\"fromacc\">")
+	for _,s := range kontonamn {
+		fmt.Fprintf(w,"    <option value=\"%s\">%s</option>", s, s)
+	}
+	fmt.Println("newtrans 40")
+	fmt.Fprintf(w,"  </select>\n")
+	fmt.Fprintf(w,"<label for=\"date\">Datum:</label>")
+	fmt.Fprintf(w,"	<input type=\"date\" id=\"date\" name=\"date\">")
+	fmt.Fprintf(w,"  <label for=\"who\">Vem:</label>")
+	fmt.Fprintf(w,"  <select name=\"who\" id=\"who\">")
+	for _,s := range personer {
+		fmt.Fprintf(w,"    <option value=\"%s\">%s</option>", s, s)
+	}
+	fmt.Fprintf(w,"  </select>\n")
+	fmt.Fprintf(w,"<label for=\"amount\">Belopp:</label>")
+	fmt.Fprintf(w,"<input type=\"number\" id=\"amount\" name=\"amount\" min=0 step=\"0.01\">")
+	fmt.Fprintf(w,"<label for=\"text\">Text:</label>")
+	fmt.Fprintf(w,"<input type=\"text\" id=\"text\" name=\"text\" >")
+	fmt.Fprintf(w,"<input type=\"submit\" value=\"Submit\"></form>\n")
+	// Överföring
+	fmt.Fprintf(w, "<h3>Överföring</h3>\n")
+	fmt.Fprintf(w,"<form method=\"POST\" action=\"/addtrans\">\n")
+	fmt.Fprintf(w,"<input type=\"hidden\" id=\"transtyp\" name=\"transtyp\" value=\"Överföring\">\n")
+	fmt.Fprintf(w,"  <label for=\"fromacc\">Från:</label>")
+	fmt.Fprintf(w,"  <select name=\"fromacc\" id=\"fromacc\">")
+	for _,s := range kontonamn {
+		fmt.Fprintf(w,"    <option value=\"%s\">%s</option>", s, s)
+	}
+	fmt.Fprintf(w,"  </select>\n")
+	fmt.Fprintf(w,"  <label for=\"toacc\">Till:</label>")
+	fmt.Fprintf(w,"  <select name=\"toacc\" id=\"toacc\">")
+	for _,s := range kontonamn {
+		fmt.Fprintf(w,"    <option value=\"%s\">%s</option>", s, s)
+	}
+	fmt.Println("newtrans 40")
+	fmt.Fprintf(w,"  </select>\n")
+	fmt.Fprintf(w,"<label for=\"date\">Datum:</label>")
+	fmt.Fprintf(w,"	<input type=\"date\" id=\"date\" name=\"date\">")
+	fmt.Fprintf(w,"  <label for=\"who\">Vem:</label>")
+	fmt.Fprintf(w,"  <select name=\"who\" id=\"who\">")
+	for _,s := range personer {
+		fmt.Fprintf(w,"    <option value=\"%s\">%s</option>", s, s)
+	}
+	fmt.Fprintf(w,"  </select>\n")
+	fmt.Fprintf(w,"<label for=\"amount\">Belopp:</label>")
+	fmt.Fprintf(w,"<input type=\"number\" id=\"amount\" name=\"amount\" min=0 step=\"0.01\">")
+	fmt.Fprintf(w,"<label for=\"text\">Text:</label>")
+	fmt.Fprintf(w,"<input type=\"text\" id=\"text\" name=\"text\" >")
+	fmt.Fprintf(w,"<input type=\"submit\" value=\"Submit\"></form>\n")
+	fmt.Println("newtrans 50")
+
+	fmt.Fprintf(w, "<a href=\"summary\">Översikt</a>\n")
+	fmt.Fprintf(w, "</body>\n")
+	fmt.Fprintf(w, "</html>\n")
+}
+
+func addtransaction(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "<html>\n")
+	fmt.Fprintf(w, "<head>\n")
+	fmt.Fprintf(w, "<style>\n")
+	fmt.Fprintf(w, "table,th,td { border: 1px solid black }\n")
+	fmt.Fprintf(w, "</style>\n")
+	fmt.Fprintf(w, "</head>\n")
+	fmt.Fprintf(w, "<body>\n")
+	fmt.Fprintf(w, "<h1>%s</h1>\n", currentDatabase)
+
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	transtyp := req.FormValue("transtyp")
+	date := req.FormValue("date")
+	who := req.FormValue("who")
+	amount := req.FormValue("amount")
+	text := req.FormValue("text")
+	fmt.Println("Val: ", transtyp)
+	fmt.Println("Val: ", date)
+	fmt.Println("Val: ", who)
+	fmt.Println("Val: ", amount)
+	fmt.Println("Val: ", text)
+
+	if transtyp == "Inköp" {
+		fromacc := req.FormValue("fromacc")
+		place := req.FormValue("place")
+		what := req.FormValue("what")
+		fmt.Println("Val: ", fromacc)
+		fmt.Println("Val: ", place)
+		fmt.Println("Val: ", what)
+
+		fmt.Fprintf(w, "Inserting... ")
+
+		sqlStatement := `
+INSERT INTO Transaktioner (FrånKonto,TillKonto,Typ,Datum,Vad,Vem,Belopp,"Text")
+VALUES (?,?,?,?,?,?,?,?)`
+		_, err = db.Exec(sqlStatement, fromacc, place, transtyp, date, what, who, strings.ReplaceAll(amount, ".", ","), text)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Fprintf(w, "Inserted.\n")
+		
+		fmt.Fprintf(w," Insert res:\n", err)
+	}
+	if transtyp == "Insättning" {
+		toacc := req.FormValue("toacc")
+		what := req.FormValue("what")
+		fmt.Println("Val: ", toacc)
+		fmt.Println("Val: ", what)
+
+		fmt.Fprintf(w, "Inserting... ")
+
+		sqlStatement := `
+INSERT INTO Transaktioner (FrånKonto,TillKonto,Typ,Datum,Vad,Vem,Belopp,"Text")
+VALUES (?,?,?,?,?,?,?,?)`
+		_, err = db.Exec(sqlStatement, "---", toacc, transtyp, date, what, who, strings.ReplaceAll(amount, ".", ","), text)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Fprintf(w, "Inserted.\n")
+		
+		fmt.Fprintf(w," Insert res:\n", err)
+	}
+	if transtyp == "Uttag" {
+		fromacc := req.FormValue("fromacc")
+		what := req.FormValue("what")
+		fmt.Println("Val: ", fromacc)
+		fmt.Println("Val: ", what)
+
+		fmt.Fprintf(w, "Inserting... ")
+
+		sqlStatement := `
+INSERT INTO Transaktioner (FrånKonto,TillKonto,Typ,Datum,Vad,Vem,Belopp,"Text")
+VALUES (?,?,?,?,?,?,?,?)`
+		_, err = db.Exec(sqlStatement, fromacc, "Plånboken", transtyp, date, "---", who, strings.ReplaceAll(amount, ".", ","), text)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w, "Inserted.\n")
+		
+		fmt.Fprintf(w," Insert res:\n", err)
+	}
+	if transtyp == "Överföring" {
+		fromacc := req.FormValue("fromacc")
+		toacc := req.FormValue("toacc")
+		fmt.Println("Val: ", fromacc)
+		fmt.Println("Val: ", toacc)
+
+		fmt.Fprintf(w, "Inserting... ")
+
+		sqlStatement := `
+INSERT INTO Transaktioner (FrånKonto,TillKonto,Typ,Datum,Vad,Vem,Belopp,"Text")
+VALUES (?,?,?,?,?,?,?,?)`
+		_, err = db.Exec(sqlStatement, fromacc, toacc, transtyp, date, "---", who, strings.ReplaceAll(amount, ".", ","), text)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w, "Inserted.\n")
+		
+		fmt.Fprintf(w," Insert res:\n", err)
+	}
+
+	fmt.Fprintf(w, "<a href=\"summary\">Översikt</a>\n")
+	fmt.Fprintf(w, "</body>\n")
+	fmt.Fprintf(w, "</html>\n")
+}
