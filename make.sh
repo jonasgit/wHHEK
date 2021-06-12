0bebd54b226befa96be663a6511290a8f63b6db1
Jonas Svensson <github@jonass.user.lysator.liu.se>
2021-06-12 20:51:00 +0200

    Initial support for Mac&Linux

---
+++ make.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export JETFILE=nojetdb.go
+
+#  SET BUILDCMD=test -v
+#  set TESTFILES=main_test.go personer_test.go konton_test.go transaktioner_test.go
+
+export BUILDCMD=build
+export TESTFILES=
+
+#  SET LINKCMD=-ldflags="-s -w"
+export LINKCMD=
+
+ECHO Hämtar beroenden...
+
+go get github.com/mattn/go-sqlite3
+go get golang.org/x/text/encoding/charmap
+go get github.com/shopspring/decimal
+
+ECHO Bygger...
+
+export SOURCEFILES="main.go platser.go transaktioner.go fastatransaktioner.go personer.go konton.go budget.go"
+
+go $BUILDCMD $LINKCMD -o wHHEK $SOURCEFILES $JETFILE $TESTFILES
+
+ECHO Klar.
+
