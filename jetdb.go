532c1b23661d0ee20a9ceefb9e7f2bbd7a68d000
Jonas Svensson <github@jonass.user.lysator.liu.se>
2022-08-14 10:37:08 +0200

    Kommentarer om JetDB

--- jetdb.go
+++ jetdb.go
@@ -1,8 +1,19 @@
+//-*- coding: utf-8 -*-
 // +build windows,386
 
 package main
 
 // TODO: testa https://github.com/mattn/go-adodb
+
+// File format should be Access 1.1 or 2.0 as described by
+// http://fileformats.archiveteam.org/wiki/Access
+// that is byte 0x40A should be 8 or 9. And Engine Type should be 2 or 3.
+
+// See also https://www.loc.gov/preservation/digital/formats/fdd/fdd000462.shtml
+
+// TODO: Testa om det går att skapa en tom databas med:
+// "Provider=Microsoft.Jet.OLEDB.4.0;Jet OLEDB:Engine Type=" & Format & ";Data Source=" & DestDB
+// Där format är 2 eller 3 alltså
 
 import (
 	"database/sql"
@@ -30,7 +41,7 @@ func openJetDB(filename string, ro bool) *sql.DB {
 	db, err := sql.Open("odbc",
 		databaseAccessCommand)
 	if err != nil {
-		// Tänkbara alternativa connect-strängar om ovanstående inte fungerar:
+		// TÃ¤nkbara alternativa connect-strÃ¤ngar om ovanstÃ¥ende inte fungerar:
 		// "Driver={Microsoft Access Driver (*.mdb)};dbq="+file
 		// "Driver={MS Access Database};DBQ="
 		// "Driver={MS Access Driver (*.mdb)};dbq="+
